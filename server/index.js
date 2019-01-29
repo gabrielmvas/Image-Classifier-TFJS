69d5d79a546df1c478c965d96e8e0171da56117d
Gabriel de Medeiros Vasconcelos <P_702210@redecamara.camara.gov.br>
2019-01-29 14:33:43 -0200

    removed CORS

--- server/index.js
+++ server/index.js
@@ -2,7 +2,6 @@ const express = require('express')
 const consola = require('consola')
 const { Nuxt, Builder } = require('nuxt')
 const app = express()
-const cors = require('cors')
 
 // Import and Set Nuxt.js options
 let config = require('../nuxt.config.js')
@@ -11,12 +10,6 @@ config.dev = !(process.env.NODE_ENV === 'production')
 
 async function start() {
     // Init Nuxt.js
-    app.use(express.static('public'));
-
-    app.use(cors())
-
-    app.options('*', cors())
-
     const nuxt = new Nuxt(config)
 
     const {
@@ -32,8 +25,6 @@ async function start() {
 
     // Give nuxt middleware to express
     app.use(nuxt.render)
-
-
 
     // Listen the server
     app.listen(port, host)
