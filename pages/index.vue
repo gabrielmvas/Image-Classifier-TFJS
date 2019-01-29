5b2810400c05ba46783477e3402ae24e3104c225
Gabriel de Medeiros Vasconcelos <P_702210@redecamara.camara.gov.br>
2019-01-29 11:17:54 -0200

    <p> bug

--- pages/index.vue
+++ pages/index.vue
@@ -10,18 +10,13 @@
                 <v-divider></v-divider>
                 <v-card-text>
                     <p>
-                        This is an simple web application that uses the model
-                        MobileNet to classify/make predictions about images.
+                        This is an simple web application that uses the modelMobileNet to classify/make predictions about images.
                     </p>
                     <p>
-                        Please note that this model have been trained to classify one thousand
-                        specific classes, so do not expect very high accuracy for whatever input, 
-                        you can input images the model does not recognize, so probably you will get
-                        a wrong prediction.
+                        Please note that this model have been trained to classify one thousand specific classes, so do not expect very high accuracy for whatever input, you can input images the model does not recognize, so probably you will get a wrong prediction.
                     </p>
                     <p>
-                        In case you want to know which classes are in the model you can 
-                        click on "Classes" on the toolbar or 
+                        In case you want to know which classes are in the model you can click on "Classes" on the toolbar or 
                         <nuxt-link to="/classes" style="color: yellow"> 
                             click here. 
                         </nuxt-link>
